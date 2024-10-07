b0d4e5e49515b252c191f9304447e5be81add4c9
kxlmtdx <nailslevens@gmail.com>
2024-10-07 19:39:29 +0300

    0.7.1

--- proj/PageMain/Auto.xaml.cs
+++ proj/PageMain/Auto.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using proj.DB;
+using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.ConstrainedExecution;
 using System.Text;
@@ -25,15 +27,34 @@ namespace proj.Main
         public Auto()
         {
             InitializeComponent();
-            var marks = AppConnect.model0db.Mark.Select(m => m.Mark1).ToList();
-            var typePrivods = AppConnect.model0db.Type_Privod.Select(tp => tp.Type_Privod1).ToList();
-            var types = AppConnect.model0db.Type1.Select(t => t.Тип_машины).ToList();
-            var Year = AppConnect.model0db.Year_Car.Select(y => y.year).ToList();
+             // var marks = AppConnect.model0db.Mark.Select(m => m.Mark1).ToList();/
 
-            CmbGroup.ItemsSource = marks;
-            CmbGroup1.ItemsSource = typePrivods;
-            CmbGroup2.ItemsSource = types;
-            CmbGroup3.ItemsSource = Year;
+             /* var typePrivods = AppConnect.model0db.TypePrivod.Select(tp => tp.Type_Privod1).ToList();/
+             / var types = AppConnect.model0db.Type1.Select(t => t.Типмашины).ToList();/
+             / var Year = AppConnect.model0db.YearCar.Select(y => y.year).ToList();*/
+            CmbGroup.SelectedValuePath = "Id";
+            CmbGroup.DisplayMemberPath = "Mark1";
+            CmbGroup.ItemsSource = AppConnect.model0db.Mark.ToList();
+
+            CmbGroup1.SelectedValuePath = "Id";
+            CmbGroup1.DisplayMemberPath = "Type_Privod1";
+            CmbGroup1.ItemsSource = AppConnect.model0db.Type_Privod.ToList();
+
+            CmbGroup2.SelectedValuePath = "Id";
+            CmbGroup2.DisplayMemberPath = "Тип_Машины";
+            CmbGroup2.ItemsSource = AppConnect.model0db.Type1.ToList();
+
+            CmbGroup3.SelectedValuePath = "Id";
+            CmbGroup3.DisplayMemberPath = "Year";
+            CmbGroup3.ItemsSource = AppConnect.model0db.Year_Car.ToList();
+
+
+
+
+            /* CmbGroup.ItemsSource = marks;/
+            /CmbGroup1.ItemsSource = typePrivods;/
+            / CmbGroup2.ItemsSource = types;/
+            /CmbGroup3.ItemsSource = Year;*/
 
             FillDataGrid();
         }
@@ -58,18 +79,42 @@ namespace proj.Main
 
         private void Search_Click(object sender, RoutedEventArgs e)
         {
-            int marks = Convert.ToInt32(CmbGroup.SelectedValue);
-            carGrid.ItemsSource = AppConnect.model0db.Staff1.Where(x => x.ID_Mark == marks).ToList();
-            var output = carGrid.ItemsSource = AppConnect.model0db.Staff1.Where(x => x.ID_Mark == marks).ToList();
+            int marksId = Convert.ToInt32(CmbGroup.SelectedValue);
+            var output = AppConnect.model0db.Staff1.Where(x => x.ID_Mark == marksId).Select(x => new{CarBrand = x.Mark.Mark1}).ToList();
 
+            int privodType = Convert.ToInt32(CmbGroup1.SelectedValue);
+            var govno = AppConnect.model0db.Staff1.Where(x => x.ID_Type == privodType).Select(x => new { prType = x.Type_Privod.Type_Privod1 }).ToList();
+
+
+            int carType = Convert.ToInt32(CmbGroup2.SelectedValue);
+            var carType1 = AppConnect.model0db.Staff1.Where(x => x.ID_Type == carType).Select(x => new { carType = x.Type1.Тип_машины }).ToList();
+
+            int carYear = Convert.ToInt32(CmbGroup3.SelectedValue);
+            var ZXC = AppConnect.model0db.Staff1.Where(x => x.ID_Year_Car == carYear).Select(x => new { carYear = x.Year_Car.year }).ToList();
+
+            var carDetails = AppConnect.model0db.Staff1
+                .Where(x => x.ID_Mark == marksId && x.ID_Type == privodType && x.ID_Type == carType && x.ID_Year_Car == carYear)
+                .Select(x => new
+                {
+                    CarBrand = x.Mark.Mark1,
+                    PrivodType = x.Type_Privod.Type_Privod1,
+                    CarType = x.Type1.Тип_машины + x.Year_Car.year
+                })
+                .ToList();
+
+            // Create and populate DataTable
             DataTable dt = new DataTable();
             dt.Columns.Add("Марка авто");
+            dt.Columns.Add("Описание авто");
+            dt.Columns.Add("Цена авто");
+            dt.Columns.Add("Фото авто");
 
-            foreach (var item in )
+            foreach (var car in carDetails)
             {
-                dt.Rows.Add(item);
+                dt.Rows.Add(car.CarBrand, car.PrivodType, car.CarType);
             }
 
+            // Set the DataTable as the data source for the grid
             carGrid.ItemsSource = dt.DefaultView;
         }
     }
